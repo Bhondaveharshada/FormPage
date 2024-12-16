050fa39c689573eced06411e759520f7eb9204ce
Harshada Bhondave <manishabhondave640@gmail.com>
2024-12-16 16:16:08 +0530

    changes in both components

--- src/form/form.component.ts
+++ src/form/form.component.ts
@@ -1,97 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, Validators, FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { ReactiveFormsModule, Validators, FormGroup, FormBuilder, FormArray, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FormService } from '../services/form.service';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [RouterModule,ReactiveFormsModule,CommonModule],
+  imports: [RouterModule,ReactiveFormsModule,CommonModule,FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent implements OnInit {
 
-  userForm:any=FormGroup;
+  title: string = '';
   formLink: string | null = null;
-  fields: string[] = [];
-  
+  additionalFields: { value: string; inputType: string }[] = []; // Store fields dynamically
 
-     constructor(private _fb:FormBuilder,
-        private formService : FormService
-     ){}
+  constructor(private formService: FormService) {}
 
-     ngOnInit(){
-      this.userForm =this._fb.group({
-        title:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-        additionalFields: this._fb.array([]), // Dynamic fields
-      });
-
-     
-
-    }
-  
-    // Getter for additionalFields
-    get additionalFields(): FormArray {
-      return this.userForm.get('additionalFields') as FormArray;
-    }
-
-
-    addField(): void {
-      const newField = this._fb.group({
-        value: ['', Validators.required],
-        inputType:['',Validators.required]
-      });
-      this.additionalFields.push(newField);
-    }
-
-    addFields() {
-      this.fields.push('string'); // Add default type as 'string'
-    }
-    updateFieldType(index: number, type: string) {
-      this.fields[index] = type; // Update the selected type at the specific index
-    }
-
-     onSave(){
-      const formId = new Date().getTime();
-      const formvalue = this.userForm.value
-      console.log("formdata", formvalue);
+  ngOnInit(): void {
       
-      localStorage.setItem(`form_${formId}`, JSON.stringify(formvalue)); 
-    //  this.formLink = `${window.location.origin}/form/${formId}`;
-      // Save form data locally
-      this.formService.addFormFields(formvalue,formId).subscribe({
-        next:(res:any)=>{
-          console.log(" fields stored successfully",res.result);
-          const id = res.result._id
-          const formid = res.result.formId
-          console.log(id);
-          
-          this.formLink = `${window.location.origin}/form/${id}/${formid}`;
+  }
 
-          
-        },error :(err:any)=>{
-          console.log("errrorrr");
-          
-        }
+  addField(): void {
+    // Add a new field with default values
+    this.additionalFields.push({ value: '', inputType: 'text' });
+  }
 
+  updateFieldType(index: number, type: string): void {
+    // Update the selected input type for a specific field
+    this.additionalFields[index].inputType = type;
+  }
 
-      })
-  
-    
-      
-      //console.log(formData.value);
-     // console.log(this.regForm.value);
-     // console.log(formData.valid);
-      
-     // console.log(this.regForm.get('fname').value);
-      // this.regForm.reset({
-      //   'fname':"harshada"
-      // })
-      
-    }
+  onSave(): void {
+    const formId = new Date().getTime();
+    const formData = {
+      title: this.title,
+      additionalFields: this.additionalFields,
+    };
 
-    fillData(){
+    console.log('Form Data:', formData);
+
+    // Store the form data in localStorage
+    localStorage.setItem(`form_${formId}`, JSON.stringify(formData));
+
+    // Save form data via service
+    this.formService.addFormFields(formData, formId).subscribe({
+      next: (res: any) => {
+        console.log('Fields stored successfully:', res.result);
+        const id = res.result._id;
+        const formId = res.result.formId;
+        this.formLink = `${window.location.origin}/form/${id}/${formId}`;
+      },
+      error: (err: any) => {
+        console.error('Error:', err);
+      },
+    });
+  }
+
+  /*   fillData(){
       this.userForm.setValue({  //or patchValue() but for setValue we cannot miss even singal control unlike patchValue.
         id:101,
         fname:'Harshada',
@@ -101,5 +68,5 @@ export class FormComponent implements OnInit {
       })
 
     }
-
+ */
 }
